77d8a08e63f34345c8040b482aac6fe81b24c36d
vitorcastilho <vitorcastilho@msn.com>
2020-12-14 10:05:22 -0300

    Curso React + Redux

---
+++ src/config/routes.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const auth = require('./auth')
+
+module.exports = function(server) {
+    // Sem proteção
+    // Definir url base para todas as rotas
+    // const router = express.Router()
+    // server.use('/api', router)
+
+    // Rotas de Ciclo de Pagamento
+    // const BillingCycle = require('../api/billingCycle/billingCycleService')
+    // BillingCycle.register(router, '/billingCycles')
+    // Sem proteção
+
+    /*
+  * Rotas protegidas por Token JWT
+  */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+    protectedApi.use(auth)
+    const BillingCycle = require('../api/billingCycle/billingCycleService')
+    BillingCycle.register(protectedApi, '/billingCycles')
+    /*
+    * Rotas abertas
+    */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+
+}
